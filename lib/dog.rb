73c191c23990f5fac9594207a0d87c36e53ff7fa
Johann Kerr <johannkerr@gmail.com>
2017-05-12 14:35:50 -0400

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,2 +1,21 @@
-class Dog
+require 'pry'
+
+class Dog < ActiveRecord::Base
+  attr_accessor :breed
+
+
+#name: 'Teddy', breed: 'cockapoo'
+
+  def self.find_or_create_by(name:, breed:)
+
+      dog = Dog.find_by_name(name)
+      dog || Dog.create(name: name, breed: breed)
+
+      # if dog
+      #   dog
+      # else
+      #   Dog.create(name: name, breed: breed)
+      # end
+  end
+
 end
